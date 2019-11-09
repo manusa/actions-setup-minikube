08a36b035943c3aefc227f1c9584cd5e79923f5c
Marc Nuri <marc@marcnuri.com>
2019-11-09 16:35:34 +0100

    #1 WIP

--- src/configure-environment.js
+++ src/configure-environment.js
@@ -7,9 +7,16 @@ const configureEnvironment = () => {
   core.info('Updating Environment configuration to support Minikube');
   child_process.execSync(
     `
-        sudo apt-get update \
-        && sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils \
-        && sudo adduser $(id -un) libvirt`,
+    apt-get purge docker docker-engine docker.io containerd runc
+    && sudo rm -rf /var/lib/docker \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+    && sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable" \
+    && sudo apt-get update \
+    && sudo apt-get install docker-ce docker-ce-cli
+    `,
     {
       stdio: 'inherit'
     }
