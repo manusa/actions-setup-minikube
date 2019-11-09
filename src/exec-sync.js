5111a4339ff0f6c6d66d115a4c62da6552e88bc5
Marc Nuri <marc@marcnuri.com>
2019-11-09 17:29:37 +0100

    #1 WIP initial implementation with non-configured Docker environment

---
+++ src/exec-sync.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const child_process = require('child_process');
+
+const execSync = command =>
+  child_process.execSync(command, {
+    stdio: 'inherit'
+  });
+
+module.exports = execSync;
--- src/install.js
+++ src/install.js
@@ -1,28 +1,20 @@
 'use strict';
 
 const core = require('@actions/core');
-const child_process = require('child_process');
+const execSync = require('./exec-sync');
 const path = require('path');
 const io = require('@actions/io');
 
 const install = async (minikube, inputs) => {
   core.info('Installing Minikube');
-  child_process.execSync(`chmod +x ${minikube}`);
+  execSync(`chmod +x ${minikube}`);
   const minikubeDirectory = path.dirname(minikube);
   await io.mv(minikube, path.join(minikubeDirectory, 'minikube'));
   core.exportVariable('MINIKUBE_HOME', minikubeDirectory);
   core.addPath(minikubeDirectory);
-  child_process.execSync(
-    `
-    sudo -E ./minikube start --vm-driver=none --kubernetes-version ${inputs.kubernetesVersion} \
-    && sudo chmod -R a+r /home/runner/.kube /home/runner/.minikube:
-    `,
-    {
-      stdio: 'inherit'
-    }
-  );
-  const minikubeVersion = child_process
-    .execSync(`minikube version`)
+  execSync(`sudo -E ./minikube start --vm-driver=none --kubernetes-version ${inputs.kubernetesVersion}`);
+  execSync(`sudo chmod -R a+r /home/runner/.kube /home/runner/.minikube`);
+  const minikubeVersion = execSync(`minikube version`)
     .toString()
     .replace(/[\n\r]/g, '');
   console.log(`${minikubeVersion} installed successfully`);
