c14d6259a2fca9fa9a267819a3cc8141cf836058
Marc Nuri <marc@marcnuri.com>
2019-11-09 16:25:50 +0100

    #1 WIP

--- src/configure-environment.js
+++ src/configure-environment.js
@@ -4,9 +4,12 @@ const core = require('@actions/core');
 const child_process = require('child_process');
 
 const configureEnvironment = () => {
-  core.info('Updating Docker configuration to support Minikube');
+  core.info('Updating Environment configuration to support Minikube');
   child_process.execSync(
-    `sudo apt-get update && sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils`,
+    `
+        sudo apt-get update \
+        && sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils \
+        && sudo adduser $(id -un) libvirt`,
     {
       stdio: 'inherit'
     }
