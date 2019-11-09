94f4ee539c5ba9e155618c6c7bcd726dbec19be7
Marc Nuri <marc@marcnuri.com>
2019-11-09 09:37:18 +0100

    #1 WIP

--- src/configure-environment.js
+++ src/configure-environment.js
@@ -3,14 +3,14 @@
 const core = require('@actions/core');
 const child_process = require('child_process');
 
-const configureDocker = () => {
+const configureEnvironment = () => {
   core.info('Updating Docker configuration to support Minikube');
   child_process.execSync(
-    `sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils`,
+    `sudo apt-get update && sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils`,
     {
       stdio: 'inherit'
     }
   );
 };
 
-module.exports = configureDocker;
+module.exports = configureEnvironment;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
 
 const errorHandler = require('./error-handler');
 const checkEnvironment = require('./check-environment');
-const configureDocker = require('./configure-docker');
+const configureEnvironment = require('./configure-environment');
 const loadInputs = require('./load-inputs');
 const download = require('./download');
 const install = require('./install');
 
 const run = async () => {
   checkEnvironment();
-  configureDocker();
+  configureEnvironment();
   const inputs = loadInputs();
   const downloadedFile = await download(inputs);
   await install(downloadedFile, inputs);
