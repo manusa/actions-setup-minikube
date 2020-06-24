941fa93ead7bccac9869a13bff66cbd3fa16a79b
Marc Nuri <marc@marcnuri.com>
2020-06-24 07:54:14 +0200

    feat: Configurable minikube start optional arguments

--- src/load-inputs.js
+++ src/load-inputs.js
@@ -11,6 +11,7 @@ const loadInputs = () => {
   });
   result.githubToken = core.getInput('github token');
   result.driver = core.getInput('driver');
+  result.startArgs = core.getInput('start args');
   return result;
 };
 
