0f23c5427e50fbd57aa6034e8855d26c12984a81
Marc Nuri <marc@marcnuri.com>
2020-06-24 07:05:18 +0200

    feat: Configurable minikube driver

--- src/__tests__/load-inputs.test.js
+++ src/__tests__/load-inputs.test.js
@@ -9,8 +9,7 @@ describe('load-inputs module test suite', () => {
       // Given
       process.env = {
         INPUT_MINIKUBE_VERSION: 'v1.33.7',
-        INPUT_KUBERNETES_VERSION: 'v1.3.37',
-        INPUT_GITHUB_TOKEN: 'secret-token'
+        INPUT_KUBERNETES_VERSION: 'v1.3.37'
       };
       // When
       const result = loadInputs();
@@ -18,10 +17,37 @@ describe('load-inputs module test suite', () => {
       expect(result).toEqual({
         minikubeVersion: 'v1.33.7',
         kubernetesVersion: 'v1.3.37',
-        githubToken: 'secret-token'
+        githubToken: '',
+        driver: ''
+      });
+    });
+    test('All variables in env, should return valid inputs', () => {
+      // Given
+      process.env = {
+        INPUT_MINIKUBE_VERSION: 'v1.33.7',
+        INPUT_KUBERNETES_VERSION: 'v1.3.37',
+        INPUT_GITHUB_TOKEN: 'secret-token',
+        INPUT_DRIVER: 'minikube-driver'
+      };
+      // When
+      const result = loadInputs();
+      // Then
+      expect(result).toEqual({
+        minikubeVersion: 'v1.33.7',
+        kubernetesVersion: 'v1.3.37',
+        githubToken: 'secret-token',
+        driver: 'minikube-driver'
       });
     });
     test('Required variables NOT in env, should throw error', () => {
+      // Given
+      process.env = {};
+      // When - Then
+      expect(loadInputs).toThrow(
+        'Input required and not supplied: minikube version'
+      );
+    });
+    test('Empty env, should throw error', () => {
       // Given
       process.env = {};
       // When - Then
