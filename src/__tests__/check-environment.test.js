2a4e3d5a11623d32a6f8770222ada6f810e624c1
Marc Nuri <marc@marcnuri.com>
2022-05-10 13:18:08 +0200

    feat: support for Ubuntu 22.04

--- src/__tests__/check-environment.test.js
+++ src/__tests__/check-environment.test.js
@@ -14,7 +14,7 @@ describe('check-environment module test suite', () => {
       process.platform = 'win32';
       // When - Then
       expect(checkEnvironment).toThrow(
-        'Unsupported OS, action only works in Ubuntu 18'
+        'Unsupported OS, action only works in Ubuntu 18, 20, or 22'
       );
     });
     test('OS is Linux but not Ubuntu, should throw Error', () => {
@@ -24,7 +24,7 @@ describe('check-environment module test suite', () => {
       fs.readFileSync.mockImplementation(() => 'SOME DIFFERENT OS');
       // When - Then
       expect(checkEnvironment).toThrow(
-        'Unsupported OS, action only works in Ubuntu 18'
+        'Unsupported OS, action only works in Ubuntu 18, 20, or 22'
       );
       expect(fs.existsSync).toHaveBeenCalled();
       expect(fs.readFileSync).toHaveBeenCalledTimes(0);
@@ -36,7 +36,7 @@ describe('check-environment module test suite', () => {
       fs.readFileSync.mockImplementation(() => 'SOME DIFFERENT OS');
       // When - Then
       expect(checkEnvironment).toThrow(
-        'Unsupported OS, action only works in Ubuntu 18'
+        'Unsupported OS, action only works in Ubuntu 18, 20, or 22'
       );
       expect(fs.existsSync).toHaveBeenCalled();
       expect(fs.readFileSync).toHaveBeenCalled();
@@ -62,6 +62,19 @@ describe('check-environment module test suite', () => {
         () => `
         NAME="Ubuntu"
         VERSION="20.04.1 LTS (Focal Fossa)"
+        `
+      );
+      // When - Then
+      expect(checkEnvironment).not.toThrow();
+    });
+    test('OS is Linux and Ubuntu 22, should not throw Error', () => {
+      // Given
+      Object.defineProperty(process, 'platform', {value: 'linux'});
+      fs.existsSync.mockImplementation(() => true);
+      fs.readFileSync.mockImplementation(
+        () => `
+        NAME="Ubuntu"
+        VERSION="22.04.1 LTS (Jammy Jellyfish)"
         `
       );
       // When - Then
