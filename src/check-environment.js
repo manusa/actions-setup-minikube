f684803a810478249ebb2c095cf496f06ee9a747
Marc Nuri <marc@marcnuri.com>
2022-05-10 13:18:08 +0200

    feat: support for Ubuntu 22.04

--- src/check-environment.js
+++ src/check-environment.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 const isLinux = () => process.platform.toLowerCase().indexOf('linux') === 0;
-const isUbuntu = version => {
+const isUbuntu = version => () => {
   const osRelease = '/etc/os-release';
   const osInfo = fs.existsSync(osRelease) && fs.readFileSync(osRelease);
   return (
@@ -12,12 +12,11 @@ const isUbuntu = version => {
     osInfo.indexOf(`VERSION="${version}`) >= 0
   );
 };
-const isUbuntu18 = () => isUbuntu('18');
-const isUbuntu20 = () => isUbuntu('20');
-const isValidLinux = () => isLinux() && (isUbuntu18() || isUbuntu20());
+['18', '20', '22'].some(v => isUbuntu(v)())
+const isValidLinux = () => isLinux() && ['18', '20', '22'].some(v => isUbuntu(v)());
 const checkOperatingSystem = () => {
   if (!isValidLinux()) {
-    throw Error('Unsupported OS, action only works in Ubuntu 18 or 20');
+    throw Error('Unsupported OS, action only works in Ubuntu 18, 20, or 22');
   }
 };
 
