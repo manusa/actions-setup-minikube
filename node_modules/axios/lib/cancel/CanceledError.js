251b09e467a573e4ddcb8aba966c913825a61298
Marc Nuri <marc@marcnuri.com>
2022-10-30 07:15:24 +0100

    deps: bump npm dependencies

--- node_modules/axios/lib/cancel/CanceledError.js
+++ node_modules/axios/lib/cancel/CanceledError.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var AxiosError = require('../core/AxiosError');
-var utils = require('../utils');
+import AxiosError from '../core/AxiosError.js';
+import utils from '../utils.js';
 
 /**
  * A `CanceledError` is an object that is thrown when an operation is canceled.
  *
- * @class
  * @param {string=} message The message.
+ * @param {Object=} config The config.
+ * @param {Object=} request The request.
+ *
+ * @returns {CanceledError} The created error.
  */
-function CanceledError(message) {
+function CanceledError(message, config, request) {
   // eslint-disable-next-line no-eq-null,eqeqeq
-  AxiosError.call(this, message == null ? 'canceled' : message, AxiosError.ERR_CANCELED);
+  AxiosError.call(this, message == null ? 'canceled' : message, AxiosError.ERR_CANCELED, config, request);
   this.name = 'CanceledError';
 }
 
@@ -19,4 +22,4 @@ utils.inherits(CanceledError, AxiosError, {
   __CANCEL__: true
 });
 
-module.exports = CanceledError;
+export default CanceledError;
